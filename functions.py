7cfaf696882677f93c0500d896889dc2d5aa7d62
JBLOZ <123325702+JBLOZ@users.noreply.github.com>
2023-11-30 17:35:47 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -248,11 +248,6 @@ def aleatorio(x):
     for i in range(x):
         num=num + str(random.randint(0,9))
 
-informacion_clientes= [{'usuario':'Hugo','contraseña':'micaela','numero de tarjeta':'0123456789012345','saldo':500,'balance del mes':-500},
-                       {'usuario':'Jordi','contraseña':'hola','numero de tarjeta':'9876543210987654','saldo':700,'balance del mes':-250},
-                       {'usuario':'Adrian','contraseña':'rana','numero de tarjeta':'0246802468024680','saldo':900,'balance del mes':-400},
-                       {'usuario':'David','contraseña':'silla','numero de tarjeta':'9753197531953197','saldo':250,'balance del mes':-800},
-                       {'usuario':'Pepe','contraseña':'hola93','numero de tarjeta':'0918273645546372','saldo':340,'balance del mes':-1000}]
 
 def menu(informacion_clientes):
     print('Menú de opciones:')
